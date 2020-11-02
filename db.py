63594a9fba0b67f46b9bf79936fa886e7c329ee1
Martin <martin.khapherian@gmail.com>
2020-11-02 17:31:15 +0100

    bdd-complait

--- db.py
+++ db.py
@@ -4,14 +4,10 @@ conn = sqlite3.connect("pylyre.sqlite")
 
 cursor = conn.cursor()
 
-sql_query = """ CREATE TABLE users(
+sql_query = """ CREATE TABLE tracks_liked(
            id integer PRIMARY KEY,
-           first_name String(120),
-           last_name String(120),
-           pseudo String(120),
-           email String(120) UNIQUE,
-           password String(120),
-           date_last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+           id_user integer,
+           id_track integer
            
 ) """
 
